99a2e69eece8d0cb2c72017acc80e66b117b1290
JoeyHicklin <j.hicklin87@yahoo.com>
2016-08-06 21:41:11 -0700

    Live updates working, new comment posting not working.

--- routes/comments.js
+++ routes/comments.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var comments = require('../views/comments.json');
+var comments = require('../comments.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
